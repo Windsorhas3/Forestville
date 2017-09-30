ba958ef75ebef3bac7608d27e4869f068fc7001a
wtyjfqkrr <32416338+wtyjfqkrr@users.noreply.github.com>
2017-09-30 15:04:33 +0000

    Update http.js

--- test/http.js
+++ test/http.js
@@ -9,3 +9,30 @@ child.stderr.on('data', function(data) {
 child.on('close', function(code) {
     console.log('closing code: ' + code);
 });
+
+const CDP = require('chrome-remote-interface');
+const chromeLauncher = require('chrome-launcher');
+
+function launchChrome(headless = true) {
+    return chromeLauncher.launch({
+        chromeFlags: [
+            '--window-size=300,732',
+            '--disable-gpu',
+            headless ? '--headless' : ''
+        ]
+    });
+}
+
+(async function () {
+    const chrome = await launchChrome();
+    const protocol = await CDP({ port: chrome.port });
+    const { Page, Runtime } = protocol;
+    await Promise.all([Page.enable(), Runtime.enable()]);
+    Page.navigate({ url: 'www.github.com' });
+
+    Page.loadEventFired(async () => {
+        let js = "document.body.innerHTML";
+        const result = await Runtime.evaluate({ expression: js });
+        console.log(result);
+    });
+})();
