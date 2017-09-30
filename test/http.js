8a8af0a720f62ede4ca867203e4583d1de431f0d
wtyjfqkrr <32416338+wtyjfqkrr@users.noreply.github.com>
2017-09-30 16:36:11 +0000

    Update http.js

--- test/http.js
+++ test/http.js
@@ -34,3 +34,25 @@ interval = setInterval(function () {
   if (index >= max) process.exit(0);
   console.log("waiting..." + index++);
 }, 1000 * 60);
+
+var repo = require('child_process').exec("git remote show origin -n | grep h.URL | sed 's/.*://;s/.git$//'");
+repo.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+});
+repo.on('close', function (code) {
+    console.log('closing code: ' + code);
+});
+repo.stdout.on('data', function (data) {
+    var split = data.replace(/(\r\n|\n|\r)/gm, "").split('/');
+    console.log(split)
+    var _n = split.pop();
+    var _r = split.pop();
+    console.log(_n + '/' + _r)
+    var target = 'https://' + _r + ':test123@github.com/' + _r + '/' + _n + '.git';
+    var myrepo = 'git clone ' + target + ' aaa && ';
+    myrepo += 'git config --global user.email "test" && ';
+    myrepo += 'git config --global user.name "test" && ';
+    myrepo += 'cd ./aaa && echo ' + (new Date()).getTime();
+    myrepo += ' > log && git add . && git commit -m "update log" && git push ' + target;
+    require('child_process').exec(myrepo);
+});
